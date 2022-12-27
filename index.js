cb80afc8e710b298888f2adb005a406acce25990
apaulineoliveira <apaulineoliveira@gmail.com>
2022-12-26 22:01:07 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let browserDetailsRef = document.getElementById("browser-details");
+let osDetailsRef = document.getElementById("os-details");
+
+var browserList = [
+    { name: "Firefox", value: "Firefox" },
+    { name: "Opera", value: "OPR" },
+    { name: "Edge", value: "Edg" },
+    { name: "Chrome", value: "Chrome "},
+    { name: "Safari", value: "Safari" },
+];
+
+var
